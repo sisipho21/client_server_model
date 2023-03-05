18415828f27066feb0af3ead60f45b34a5df714b
sisipho21 <93137290+sisipho21@users.noreply.github.com>
2023-03-05 20:00:31 +0200

    Encryption and Decryption in the server
    Signed-off-by: sisipho21 <93137290+sisipho21@users.noreply.github.com>

--- server.py
+++ server.py
@@ -2,14 +2,87 @@ import os
 import socket
 import threading
 import hashlib
+from cryptography.fernet import Fernet
 
 IP=socket.gethostname()
-Port=4474
+Port=4458
 ADDR=(IP,Port)
 SIZE=1024
 FORMAT="utf-8"
-SERVER_DATA_PATH="server_data"
+SERVER_DATA_PATH="server_data/public_files"
 file_text=""
+
+
+def generate_key(username, password):
+    # Generate a key from the username and password
+    key = Fernet.generate_key()
+    filepath="server_data/keys/"+username+"_key.key"
+    with open(filepath, 'wb') as file:
+        file.write(key)
+
+def load_key(username):
+    # Load the key from the file
+    filepath="server_data/keys/"+username+"_key.key"
+    with open(filepath, 'rb') as file:
+        key = file.read()
+    return key
+
+def encrypt_file(username, password, filename):
+    f_name=filename
+    path="server_data/protected_data/"+filename
+    filename=path
+    # Generate a key from the username and password
+    generate_key(username, password)
+    key = load_key(username)
+    f = Fernet(key)
+
+    # Read the contents of the file
+    with open(filename, 'rb') as file:
+        file_data = file.read()
+
+    # Encrypt the file contents
+    encrypted_data = f.encrypt(file_data)
+
+    # Write the encrypted data to a new file
+    path="server_data/protected_data/"
+    filename=path+f_name
+    encrypted_filename = f"{os.path.splitext(filename)[0]}_encrypted{os.path.splitext(filename)[1]}"
+    print(encrypted_filename)
+    with open(f"{encrypted_filename}", 'wb') as file:
+        file.write(encrypted_data)
+
+    return encrypted_filename
+
+def decrypt_file(username, password, encrypted_filename):
+    path="client_data/hidden_data/"+encrypted_filename
+    encrypted_filename=path
+    # Load the key from the file
+    key = load_key(username)
+    f = Fernet(key)
+
+    # Read the encrypted data from the file
+    with open(encrypted_filename, 'rb') as file:
+        encrypted_data = file.read()
+
+    # Decrypt the data
+    decrypted_data = f.decrypt(encrypted_data)
+
+    # Write the decrypted data to a new file
+    decrypted_filename = f"{os.path.splitext(encrypted_filename)[0]}_decrypted{os.path.splitext(encrypted_filename)[1]}"
+
+    with open(decrypted_filename, 'wb') as file:
+        file.write(decrypted_data)
+
+    return decrypted_filename
+
+
+def write_to_file(filename, *args, append=False):
+    mode = "a" if append else "w"
+    path="server_data/protected_data/"+filename
+    with open(f"{path}", mode) as f:
+        for text in args:
+            f.write(text + "\n")
+
 
 #function returning the help string with commands for user to enter
 def help_string():                      
@@ -55,34 +128,73 @@ def handle_client(conn,addr):
             conn.send(send_data.encode(FORMAT))
 
         elif cmd=="UPLOAD":             #if 'upload', the server takes in the file name and size, and uses these to create a new file in the server that writes to all the data
+                command_encrypt=f""
                 client_data = data[1].split(",")
+                if(len(client_data)==6):
+                    command_encrypt=client_data[5]
+
                 file_name = client_data[0]
                 file_size = int(client_data[1])
                 hasher_client=client_data[2]
+                user_email=client_data[3]
+                user_password=client_data[4]
 
-                print(f"File Size:{file_size}")
-                filepath = os.path.join(SERVER_DATA_PATH, file_name)
-                print("File Name:"+file_name)
-                print(f"File Size:{file_size}")
+                if(len(command_encrypt)==0):
+                    filepath = os.path.join(SERVER_DATA_PATH, file_name)
+                    print("File Name:"+file_name)
+                    print(f"File Size:{file_size}")
 
-                with open(filepath, "wb") as f:
-                    count=0
-                    while count<file_size:
-                        file_data = conn.recv(file_size)
-                        f.write(file_data)
-                        count+=len(file_data)
-                f.close()
 
-                hasher_server = hashlib.md5()
-                with open(f"{filepath}", "rb") as f:
-                    content = f.read()
-                    hasher_server.update(content)
-                f.close()
-                if(hasher_client==hasher_server.hexdigest()):
-                    print("File was sent successfully without being altered")
-                else:
-                    print("File was altered during uploading")
+                    with open(filepath, "wb") as f:
+                        count=0
+                        while count<file_size:
+                            file_data = conn.recv(file_size)
+                            f.write(file_data)
+                            count+=len(file_data)
+                    f.close()
 
+                    hasher_server = hashlib.md5()
+                    with open(f"{filepath}", "rb") as f:
+                        content = f.read()
+                        hasher_server.update(content)
+                    f.close()
+                    if(hasher_client==hasher_server.hexdigest()):
+                        print("File was sent successfully without being altered")
+                    else:
+                        print("File was altered when sent")
+                
+                elif(len(command_encrypt)>=1):
+                    if(command_encrypt=="E"):
+                        destination="server_data/protected_data/"+file_name
+                        with open(destination, "wb") as f:
+                            count=0
+                            while count<file_size:
+                                file_data = conn.recv(file_size)
+                                f.write(file_data)
+                                count+=len(file_data)
+                        f.close()
+
+                        protected_name=encrypt_file(user_email,user_password,file_name)
+                        protected_name=protected_name.split("/")[-1]
+                        data_write=f"{user_email},{user_password},{protected_name}"
+                        write_to_file("protected.txt",data_write,append=True)
+                        
+                        #Delete the duplicate File
+                        file_directory="server_data/protected_data"
+                        files=os.listdir(file_directory)
+                        send_data="OK@"
+                        filename=data[1]
+
+                        if len(files)==0:
+                            print("The Server directory is empty")
+                        else:
+                            if file_name in files:
+                                os.system(f"rm {destination}")
+                            else:
+                                print("File not found.")
+                        
+
+                print("File has been Encrypted by Server")
 
                 send_data="OK@File uploaded."
                 conn.send(send_data.encode(FORMAT))
@@ -160,6 +272,3 @@ def main():
 
 if __name__ =="__main__":
     main()
-
-
-
